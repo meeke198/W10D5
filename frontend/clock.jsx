abe4f42c2b5f6fc95d38c8cbd47b28675709975b
hien nguyen <thuhiennguyen198@gmail.com>
2021-10-15 16:45:23 -0400

    add index.css

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -7,14 +7,42 @@ class Clock extends React.Component {
         this.state = {
             time: new Date()
         }
+        this.tick = this.tick.bind(this);
+    }
+
+    componentDidMount(){
+        this.intervalId = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervalId);
+    }
+    tick(){
+        this.setState({time: new Date()});
     }
 
     render(){
+        let hours = this.state.time.getHours();
+        let minutes = this.state.time.getMinutes();
+        let seconds = this.state.time.getSeconds();
 
+        if (seconds < 10) {
+            seconds = `0${seconds}`;
+        } 
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+        if (hours < 10) {
+            hours = `0${hours}`;
+        }
         return(
             <div>
                 <h1>Clock</h1>
+                <div>
+                    <p>Time: {hours}:{minutes}:{seconds} PDT</p> 
+                </div>
             </div>
+            
         )
     }
 
